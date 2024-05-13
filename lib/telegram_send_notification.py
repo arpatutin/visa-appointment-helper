45460648011057fe5f152edb0bcb84f349c762a6
Arsenii Patutin <arpatutin@outlook.com>
2024-05-13 23:48:42 +0400

    fix

--- lib/telegram_send_notification.py
+++ lib/telegram_send_notification.py
@@ -3,5 +3,5 @@ import os
 
 bot = telebot.TeleBot(os.environ["tg_token"])
 
-bot.send_message(-1002122327420, "ПОЙМАНА ЗАПИСЬ В НЕМЕЦКОМ КОНСУЛЬСТВЕ\n@aampelas @rpatutin @younona17")
+bot.send_message(-1002122327420, "ПОЙМАНА ЗАПИСЬ В НЕМЕЦКОМ КОНСУЛЬСТВЕ")
 
